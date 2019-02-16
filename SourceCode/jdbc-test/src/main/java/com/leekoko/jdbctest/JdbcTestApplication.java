ceb6ef4754030cbb220722b1f79551121e7cd918
liyb <908230189@qq.com>
2019-02-16 22:33:08 +0800

    提交

---
+++ SourceCode/jdbc-test/src/main/java/com/leekoko/jdbctest/JdbcTestApplication.java
@@ -0,0 +1,14 @@
+package com.leekoko.jdbctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcTestApplication.class, args);
+    }
+
+}
+
---
+++ SourceCode/jdbc-test/src/test/java/com/leekoko/jdbctest/JdbcTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.leekoko.jdbctest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JdbcTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
